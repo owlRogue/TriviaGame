02ff33d49285895a61bb3154002b4ba8df7b2629
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-02 15:51:14 -0700

    late night

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,80 @@
-var images = ["images/bootstrap.png", "images/github-logo.jpg", "images/logo_JavaScript.png"];
+// https://www.w3schools.com/jsref/prop_radio_form.asp
 
-// Variable showImage will hold the setInterval when we start the slideshow
-var showImage;
+var count = count++;
+var inputQ1 = "<input type='"'radio'"' id='"'a1'"'"+count+""'>"
 
-// Count will keep track of the index of the currently displaying picture.
-var count = 0;
+q1 = "Which of these colors did King Henry XIII wear the most often?"
+a1 = ["burnt orange","navy blue","turquoise","teal"];
 
-// TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
-$("#start").click(startSlideshow);
+q2 = "Which of these colors would you wear the most often?"
+a2 = ["salmon","coral","peach","teal"];
 
-// TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
-$("#stop").click(stopSlideshow);
+q3 = "Which color is not in the rainbow?"
+a3 = ["tan","brown","white","black","none of the above"];
 
 
-// This function will replace display whatever image it's given
-// in the 'src' attribute of the img tag.
-function displayImage() {
-  $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
-}
+var x = document.createElement("INPUT");
+    x.setAttribute("label", q1)
+    x.setAttribute("type", "radio")
+    x.setAttribute("name", a1[0])
+    x.setAttribute("required", true)
+    x.setAttribute("value", 2);
 
-function nextImage() {
-  //  TODO: Increment the count by 1.
-  count++;
+    
 
-  // TODO: Show the loading gif in the "image-holder" div.
-  $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
 
-  // TODO: Use a setTimeout to run displayImage after 1 second.
-  setTimeout(displayImage, 1000);
+// var images = ["images/bootstrap.png", "images/github-logo.jpg", "images/logo_JavaScript.png"];
 
-  // TODO: If the count is the same as the length of the image array, reset the count to 0.
-  if (count === images.length) {
-    count = 0;
-  }
-}
+// // Variable showImage will hold the setInterval when we start the slideshow
+// var showImage;
 
-function startSlideshow() {
+// // Count will keep track of the index of the currently displaying picture.
+// var count = 0;
 
-  // TODO: Use showImage to hold the setInterval to run nextImage.
-  if (!showImage) {
-    showImage = setInterval(nextImage, 3000);
-  }
-}
+// // TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
+// $("#start").click(startSlideshow);
 
-function stopSlideshow() {
+// // TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
+// $("#stop").click(stopSlideshow);
 
-  // TODO: Put our clearInterval here:
-  clearInterval(showImage);
-  showImage = undefined;
 
-}
+// // This function will replace display whatever image it's given
+// // in the 'src' attribute of the img tag.
+// function displayImage() {
+//   $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
+// }
 
-// This will run the display image function as soon as the page loads.
-displayImage();
+// function nextImage() {
+//   //  TODO: Increment the count by 1.
+//   count++;
+
+//   // TODO: Show the loading gif in the "image-holder" div.
+//   $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
+
+//   // TODO: Use a setTimeout to run displayImage after 1 second.
+//   setTimeout(displayImage, 1000);
+
+//   // TODO: If the count is the same as the length of the image array, reset the count to 0.
+//   if (count === images.length) {
+//     count = 0;
+//   }
+// }
+
+// function startSlideshow() {
+
+//   // TODO: Use showImage to hold the setInterval to run nextImage.
+//   if (!showImage) {
+//     showImage = setInterval(nextImage, 3000);
+//   }
+// }
+
+// function stopSlideshow() {
+
+//   // TODO: Put our clearInterval here:
+//   clearInterval(showImage);
+//   showImage = undefined;
+
+// }
+
+// // This will run the display image function as soon as the page loads.
+// displayImage();
