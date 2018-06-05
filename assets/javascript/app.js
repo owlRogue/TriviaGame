ad7d577e50239646dec6e8552c4628da253c7623
Koltyn Palmer <koltynchiaro@gmail.com>
2018-06-05 15:40:14 -0600

    loop it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 // https://www.w3schools.com/jsref/prop_radio_form.asp
 // https://www.formget.com/create-form-using-jquery/
 
-var topic = ["sport", "store", "color", "beverage", "hour of the day", "country"]
-    sport = ["skateboarding", "roller blading", "rugby", "snowboarding"]
-    store = ["Express","Uniqlo","H&M","Forenver 21"]
-    color = ["alice blue","blue violet","sunflower yellow","midnight blue"]
-    beverage = ["coconut milk","coconut water","aloe juice","birch beer"]
-    hour = ["10:27am","12:00am","4:20pm","4:20am"]
-    country = ["Romania","Russia","Italy","Philippines"]
+var topic = ["sport", "store", "color", "beverage", "hour of the day", "country"];
+var sport = ["skateboarding", "roller blading", "rugby", "snowboarding"];
+var store = ["Express","Uniqlo","H&M","Forenver 21"];
+var color = ["alice blue","blue violet","sunflower yellow","midnight blue"];
+var beverage = ["coconut milk","coconut water","aloe juice","birch beer"];
+var hour = ["10:27am","12:00am","4:20pm","4:20am"];
+var country = ["Romania","Russia","Italy","Philippines"];
 
     var h3 = $("<h3>")
              $('#h3').text("KP Trivia");
-
-    var blue = "blue"
-    $("#test").append("KP Trivia");
+    
+    var clock = $('<div>Time Remaining: </div>')
+    var time = "00:00"
+    $('#nav').append(clock);
+    $(clock).append(time);
 
 $(document).ready(function() {
     
@@ -34,7 +36,7 @@ $(document).ready(function() {
         var question = q;
         
         for (var i = 0; i < topic.length; i++) {
-            var label = $("<p/>");
+            var label = $("<div/>");
             var topics = topic[i]
             var q = ("Koltyn's favorite " + topics + " is...")
 
@@ -45,14 +47,10 @@ $(document).ready(function() {
             answer = ('"answer"')
             id = ('"q'+i+'"')
 
+            var input = $("<input type="+radio+"id="+id+"name="+id+"></input><label class="+answer+"for="+id+">"+id+"</label>");
 
-            
-            // var input = $("<input>", {type: 'radio', id: 'a1-3', name: 'a1', value: '0'})
-            var input = $("<input type="+radio+"id="+id+"name="+id+"></input>");
-            var option = $("<label class="+answer+"for="+id+">test: 123 </label>");
-            // var blue = ('<p>blue</p>');
             $("#form").append(input);
-            $(input).append(option);
+            // $(input).append(option);
             
                 // $("#form").append(input, input2, input3, input4);
 
