f9cc0d0ec480cfb7a97657eb0a9982e58236497e
Koltyn Palmer <koltynchiaro@gmail.com>
2018-06-05 15:08:48 -0600

    styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,113 @@
 // https://www.w3schools.com/jsref/prop_radio_form.asp
+// https://www.formget.com/create-form-using-jquery/
 
-var count = count++;
-var inputQ1 = "<input type='"'radio'"' id='"'a1'"'"+count+""'>"
+var topic = ["sport", "store", "color", "beverage", "hour of the day", "country"]
+    sport = ["skateboarding", "roller blading", "rugby", "snowboarding"]
+    store = ["Express","Uniqlo","H&M","Forenver 21"]
+    color = ["alice blue","blue violet","sunflower yellow","midnight blue"]
+    beverage = ["coconut milk","coconut water","aloe juice","birch beer"]
+    hour = ["10:27am","12:00am","4:20pm","4:20am"]
+    country = ["Romania","Russia","Italy","Philippines"]
 
-q1 = "Which of these colors did King Henry XIII wear the most often?"
-a1 = ["burnt orange","navy blue","turquoise","teal"];
+    var h3 = $("<h3>")
+             $('#h3').text("KP Trivia");
 
-q2 = "Which of these colors would you wear the most often?"
-a2 = ["salmon","coral","peach","teal"];
+    var blue = "blue"
+    $("#test").append("KP Trivia");
 
-q3 = "Which color is not in the rainbow?"
-a3 = ["tan","brown","white","black","none of the above"];
-
-
-var x = document.createElement("INPUT");
-    x.setAttribute("label", q1)
-    x.setAttribute("type", "radio")
-    x.setAttribute("name", a1[0])
-    x.setAttribute("required", true)
-    x.setAttribute("value", 2);
-
+$(document).ready(function() {
     
+    var score = 0;
+    
+    var sub = $("<p>").text("Hey you! How well do you think you know me?");
+    var makeForm = $("<form>", {action: '#', method: '#'});
+    var form = $("#form").addClass("textarea");
+        $('#popQuestion').append(form);
+
+    var topics = [];
+    // var sports = [];
+    // var stores = [];
+    
+    $("#form").append(h3, sub, makeForm);
+    
+    function createForm () {
+        var question = q;
+        
+        for (var i = 0; i < topic.length; i++) {
+            var label = $("<p/>");
+            var topics = topic[i]
+            var q = ("Koltyn's favorite " + topics + " is...")
+
+            $("#form").append(label); // working
+            (label).append(q);
+            console.log(q);
+            radio = ('"radio"')
+            answer = ('"answer"')
+            id = ('"q'+i+'"')
 
 
-// var images = ["images/bootstrap.png", "images/github-logo.jpg", "images/logo_JavaScript.png"];
+            
+            // var input = $("<input>", {type: 'radio', id: 'a1-3', name: 'a1', value: '0'})
+            var input = $("<input type="+radio+"id="+id+"name="+id+"></input>");
+            var option = $("<label class="+answer+"for="+id+">test: 123 </label>");
+            // var blue = ('<p>blue</p>');
+            $("#form").append(input);
+            $(input).append(option);
+            
+                // $("#form").append(input, input2, input3, input4);
 
-// // Variable showImage will hold the setInterval when we start the slideshow
-// var showImage;
-
-// // Count will keep track of the index of the currently displaying picture.
-// var count = 0;
-
-// // TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
-// $("#start").click(startSlideshow);
-
-// // TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
-// $("#stop").click(stopSlideshow);
+       
+            }
+    }
+    createForm();
 
 
-// // This function will replace display whatever image it's given
-// // in the 'src' attribute of the img tag.
-// function displayImage() {
-//   $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
-// }
+    });
 
-// function nextImage() {
-//   //  TODO: Increment the count by 1.
-//   count++;
 
-//   // TODO: Show the loading gif in the "image-holder" div.
-//   $("#image-holder").html("<img src='images/loading.gif' width='200px'/>");
-
-//   // TODO: Use a setTimeout to run displayImage after 1 second.
-//   setTimeout(displayImage, 1000);
-
-//   // TODO: If the count is the same as the length of the image array, reset the count to 0.
-//   if (count === images.length) {
-//     count = 0;
-//   }
-// }
-
-// function startSlideshow() {
-
-//   // TODO: Use showImage to hold the setInterval to run nextImage.
-//   if (!showImage) {
-//     showImage = setInterval(nextImage, 3000);
-//   }
-// }
-
-// function stopSlideshow() {
-
-//   // TODO: Put our clearInterval here:
-//   clearInterval(showImage);
-//   showImage = undefined;
-
-// }
-
-// // This will run the display image function as soon as the page loads.
-// displayImage();
+    $(function () {
+        var i = 0;
+        var allQuestions = [{
+            question: "What is Kolt's fav sport?",
+            choices: sport,
+            correctAnswer: sport[3]
+        }, {
+            question: "What is 21x 21?",
+            choices: store,
+            correctAnswer: store[1]
+        }, {
+            question: "Which number is prime?",
+            choices: color,
+            correctAnswer: color[0]
+        }, {
+            question: "Which number is prime?",
+            choices: beverage,
+            correctAnswer: beverage[1]
+        }, {
+            question: "Which number is prime?",
+            choices: hour,
+            correctAnswer: hour[3]
+        }, {
+            question: "What is 65/5",
+            choices: country,
+            correctAnswer: country[2]
+        }];
+        
+        function populateQuestion() {
+            $("#area").text(allQuestions[i].question); //add question
+            var $area = $('#area'),
+                j = 0;
+            //add radio and answer choices
+            for (j; j < allQuestions.length; j++) {
+        
+                (function (idx) {
+                    var html = '<div class="answerRow"><input class="answerRadio" type="radio" name="question' + idx + '" value="' + allQuestions[idx].correctAnswer + '"/><label class="answerRadioLabel"  for="question' + idx + '">test: '+allQuestions[idx].choices[j]+'   </label>';
+                    $area.append(html);
+                })(j);
+            }
+        }
+        
+        populateQuestion();
+        
+        
+        });
